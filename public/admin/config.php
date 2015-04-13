9db2c1a2e49180b5533ab01ea6f6a34a02c94b09
fernandofmendes <fernando.mendes@webca.com.br>
2015-04-13 18:23:33 -0300

    upfiles

---
+++ public/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://daxam.homolog.lojavirtual.digital/admin/');
+define('HTTP_CATALOG', 'http://daxam.homolog.lojavirtual.digital/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://daxam.homolog.lojavirtual.digital/admin/');
+define('HTTPS_CATALOG', 'http://daxam.homolog.lojavirtual.digital/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/daxam/public/admin/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/daxam/public/system/');
+define('DIR_DATABASE', '/Users/fernandomendes/github/daxam/public/system/database/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/daxam/public/admin/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/daxam/public/admin/view/template/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/daxam/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/daxam/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/daxam/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/daxam/public/download/');
+define('DIR_LOGS', '/Users/fernandomendes/github/daxam/public/system/logs/');
+define('DIR_CATALOG', '/Users/fernandomendes/github/daxam/public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'daxam');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ public/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://daxam.homolog.lojavirtual.digital/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://daxam.homolog.lojavirtual.digital/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/daxam/public/catalog/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/daxam/public/system/');
+define('DIR_DATABASE', '/Users/fernandomendes/github/daxam/public/system/database/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/daxam/public/catalog/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/daxam/public/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/daxam/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/daxam/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/daxam/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/daxam/public/download/');
+define('DIR_LOGS', '/Users/fernandomendes/github/daxam/public/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'daxam');
+define('DB_PREFIX', 'oc_');
+?>
