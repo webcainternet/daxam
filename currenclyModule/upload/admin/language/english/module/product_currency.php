e746d792acb0a9ce9bc938acc582751cdeda7943
fernandofmendes <fernando.mendes@webca.com.br>
2015-05-21 12:04:57 -0300

    currency

---
+++ currenclyModule/upload/admin/language/english/module/product_currency.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']       = 'Product Currency';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module Product Currency';
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+
+// Entry
+$_['entry_code']          = 'Currency Product Code:<br /><span class="help">Select currency for products in store.</span>';
+$_['entry_layout']        = 'Layout:';
+$_['entry_position']      = 'Position:';
+$_['entry_status']        = 'Status:';
+$_['entry_sort_order']    = 'Sort Order:';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module Product Currency!';
+$_['error_code']          = 'Code Required';
+?>
